aac0aae7aba0d9830084a97c1eaac8b815121f16
AntonyanArmen <antonyanag@rambler.ru>
2017-01-22 23:54:36 +0300

    SEO optimizations

--- js/custom.js
+++ js/custom.js
@@ -2,19 +2,6 @@
 -------------------------------------------*/
 $(function(){
 
-    /* start typed element */
-    //http://stackoverflow.com/questions/24874797/select-div-title-text-and-make-array-with-jquery
-    var subElementArray = $.map($('.sub-element'), function(el) { return $(el).text(); });    
-    $(".element").typed({
-        strings: subElementArray,
-        typeSpeed: 30,
-        contentType: 'html',
-        showCursor: false,
-        loop: true,
-        loopCount: true,
-    });
-    /* end typed element */
-
     /* Smooth scroll and Scroll spy (https://github.com/ChrisWojcik/single-page-nav)    
     ---------------------------------------------------------------------------------*/ 
     $('.templatemo-nav').singlePageNav({
@@ -22,7 +9,6 @@ $(function(){
         filter: ':not(.external)',
         updateHash: false
     });
-
     /* start navigation top js */
     $(window).scroll(function(){
         if($(this).scrollTop()>58){
